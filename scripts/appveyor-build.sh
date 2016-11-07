4a8ecb7a17033af533d60f5092c345e0b3df8e27
markfirmware <markfirmware@users.noreply.github.com>
2016-11-07 11:46:00 -0500

    try build of example 01

--- scripts/appveyor-build.sh
+++ scripts/appveyor-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+mkdir output
 mkdir github.com
 mkdir github.com/ultibohub
 cd github.com/ultibohub
@@ -17,3 +18,10 @@ appveyor AddMessage "ultibo example 01 - pi2 - kernel7.img"
  @/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/RPI2.CFG \
  -O2 \
  HelloWorld.lpr
+cp -a kernel7.img /c/Projects/tbr-one/output/01-kernel7.img
+
+appveyor AddMessage "zip artifacts"
+cd /c/Projects/tbr-one/output
+7z a ../kernel7.img.zip kernel7.img >> 7z.log
+cd ..
+
