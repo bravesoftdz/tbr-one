33de38149abac4da677f8f9925a0808472172d3f
markfirmware <markfirmware@users.noreply.github.com>
2016-11-07 17:33:15 -0500

    testing raspbian reboot

--- subrepo/ultibohub/Examples/Demo.pas
+++ subrepo/ultibohub/Examples/Demo.pas
@@ -10,6 +10,8 @@ uses
 
 procedure DemoStart;
 begin
+ while not DirectoryExists('c:'\') do
+  Sleep (100);
  DeleteFile('c:\kernel7.img');
  CopyFile('c:\backup\kernel7.img','c:\kernel7.img',False);
 end;
