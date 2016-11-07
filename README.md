288204b080deb9d528b980e81ce5226774f28bf9
markfirmware <markfirmware@users.noreply.github.com>
2016-11-07 10:59:15 -0500

    appveyor.yml

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tbr-one
+# tbr-one [![Build status](https://ci.appveyor.com/api/projects/status/9bs4s8er7hb86ls0?svg=true)](https://ci.appveyor.com/project/markfirmware/tbr-one)
--- scripts/appveyor-build.sh
+++ scripts/appveyor-build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo test
+BUILDEXITCODE = 0
+
+mkdir github.com
+mkdir github.com/ultibohub
+cd github.com/ultibohub
+git clone https://github.com/ultibohub/Examples
+cd Examples/01-HelloWorld/RPi2
+
+appveyor AddMessage "ultibo example 01 - pi2 - kernel7.img"
+/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/fpc \
+ -B \
+ -Tultibo \
+ -Parm \
+ -CpARMV7A \
+ -WpRPI2B \
+ @C:\Ultibo\Core\fpc\3.1.1\bin\i386-win32\RPI2.CFG \
+ -O2 \
+ HelloWorld.lpr
