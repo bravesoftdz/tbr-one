1ab3bb8b10a309bc661e8e70a32c74cf0b834c45
markfirmware <markfirmware@users.noreply.github.com>
2016-11-07 11:58:27 -0500

    appveyor.yml

--- scripts/appveyor-build.sh
+++ scripts/appveyor-build.sh
@@ -6,20 +6,28 @@ mkdir github.com
 mkdir github.com/ultibohub
 cd github.com/ultibohub
 git clone https://github.com/ultibohub/Examples
-cd Examples/01-HelloWorld/RPi2
 
-appveyor AddMessage "ultibo example 01 - pi2 - kernel7.img"
-/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/fpc \
- -B \
- -Tultibo \
- -Parm \
- -CpARMV7A \
- -WpRPI2B \
- @/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/RPI2.CFG \
- -O2 \
- HelloWorld.lpr
-cp -a kernel7.img /c/Projects/tbr-one/output/01-kernel7.img
+function compileExampleRPi1 {
+    cd Examples/$1-$2/RPi2
+    appveyor AddMessage "compiling ultibo example $1 - pi2"
+    /c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/fpc \
+     -B \
+     -Tultibo \
+     -Parm \
+     -CpARMV7A \
+     -WpRPI2B \
+     @/c/Ultibo/Core/fpc/3.1.1/bin/i386-win32/RPI2.CFG \
+     -O2 \
+     $2.lpr
+    cp -a kernel7.img /c/Projects/tbr-one/output/$1-kernel7.img
+}
+
+compileExampleRPi2 01 HelloWorld
+compileExampleRPi2 02 Blinker
+compileExampleRPi2 03 ScreenOutput
+
+cd /c/Projects/tbr-one
 
 appveyor AddMessage "zip artifacts"
-7z a tbr-one-build.zip output/01-kernel7.img
+7z a tbr-one-build.zip output/*.img
 
